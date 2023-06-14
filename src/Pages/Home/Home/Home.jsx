41f5813f244f535100d8cc74e47cd691f020f239
Towhid-Raiyan <towhid.raiyan.com>
2023-06-14 23:56:31 +0600

    bug fixed

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,10 +3,12 @@ import Facilities from "../Facilities/Facilities";
 import PopularClasses from "../PopularClasses/PopularClasses";
 import PopularInstructor from "../PopularInstructor/PopularInstructor";
 import Reviews from "../Reviews/Reviews";
+import setTitle from "../../../hook/setTitle";
 
 const Home = () => {
+    setTitle("Home");
     return (
-        <div>
+        <div className="dark:bg-slate-800 dark:text-white">
             <Banner></Banner>
             <PopularClasses></PopularClasses>
             <PopularInstructor></PopularInstructor>
