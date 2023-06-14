5cf7201452edd7a64990e60ac05e603a0acbdda4
Towhid-Raiyan <towhid.raiyan.com>
2023-06-14 20:09:31 +0600

    instructor view courses  added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,11 +4,14 @@ import {
 import Dashboard from "../Layouts/Dashboard";
 import Main from "../Layouts/Main";
 import Classes from "../Pages/Class/Classes";
+import AddCourse from "../Pages/Dashboard/Instructor/AddCourse";
+import ViewCourses from "../Pages/Dashboard/Instructor/ViewCourses";
 import Errorpage from "../Pages/Errorpage/Errorpage";
 import Home from "../Pages/Home/Home/Home";
 import Instructors from "../Pages/Instructors/Instructors";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import InstructorRoute from "./InstructorRoute";
 import PrivateRoute from "./PrivateRoute";
 
   export const router = createBrowserRouter([
@@ -43,8 +46,15 @@ import PrivateRoute from "./PrivateRoute";
       path: 'dashboard',
       element: (<PrivateRoute><Dashboard></Dashboard></PrivateRoute>),
       children: [
-        {
 
+        //Instructor
+        {
+          path: 'addCourse',
+          element: (<InstructorRoute><AddCourse></AddCourse></InstructorRoute>)
+        },
+        {
+          path: 'viewCourse',
+          element: (<InstructorRoute><ViewCourses></ViewCourses></InstructorRoute>)
         }
       ]
     }
